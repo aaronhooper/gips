cab25efa6d5569c7bc9e8b54f62bc4d4959543b1
Aaron Hooper <rnjckhpr@gmail.com>
2019-05-12 03:30:53 +0100

    Spacing

--- geo.py
+++ geo.py
@@ -17,6 +17,7 @@ def geolocate_ips(ips):
     countries = remove_emptys([geolocate_ip(ip) for ip in ips])
     return countries
 
+
 def extract_ips(file_object):
     """
 
@@ -32,6 +33,7 @@ def extract_ips(file_object):
     ipv4_address = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
     ips = ipv4_address.findall(file_text)
     return ips
+
 
 def geolocate_ip(ip):
     """
@@ -52,6 +54,7 @@ def geolocate_ip(ip):
     logging.info("Country found: " + country)
 
     return country
+
 
 def extract_country(html):
     """
@@ -74,6 +77,7 @@ def extract_country(html):
     country_name = country_header.find_next_sibling().text.strip()
     return country_name
 
+
 def is_reserved(ip):
     """
 
@@ -85,6 +89,7 @@ def is_reserved(ip):
     """
     private_ip = re.compile(r"192\.168\.\d{1,3}\.\d{1,3}")
     return bool(private_ip.match(ip)) or ip == "127.0.0.1"
+
 
 def remove_emptys(array):
     """
